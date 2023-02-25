8b7d5c7ceb435fe883f6d95869e206a7b437b0cd
Vlad Klymchuk <klim23am@gmail.com>
2023-02-25 19:33:39 +0200

    lab2 create navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
@@ -7,13 +6,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <div>
-          <h1> Hello world!</h1>
-          <a></a>
-          <Button variant="primary">Button</Button>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        
+          <li class="nav-item">
+            <a class="nav-link"  href="">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
+    </div>
+  </nav>
   );
 }
 
-
 export default App;
+
+
+
+
