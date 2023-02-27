e6556e50c05d8ef35ca787577beda098ceb8c194
Vlad Klymchuk <klim23am@gmail.com>
2023-02-27 17:58:05 +0200

    lab2 end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import { Button } from "react-bootstrap";
+import { Button, Navbar } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from "./NavbarComp";
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -15,44 +16,21 @@ import Blog from './Pages/Blog';
 
 function App() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
+    <div className="App">
+      <NavbarComp/>
+      <Router>
 
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-          </li>
-        
-          <li class="nav-item">
-            <a class="nav-link"  href="/contacts">Contacts</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link"  href="/blog">Blog</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+        <Routes>
+            <Route path="/" element={<Home  />}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="/blog" element={<Blog/>}/>
+        </Routes>
+      </Router>
+
     </div>
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home  />}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contacts" element={<Contacts/>}/>
-          <Route path="/blog" element={<Blog/>}/>
-      </Routes>
-    </Router>
-  </nav>
+  
+
   );
 }
 
